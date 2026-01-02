e20172d53da1d2d92c74835400d3ad2094373033
harismuhammad0912 <muhammadharis091204@gmail.com>
2026-01-03 03:18:34 +0700

    implement DefaultContainerApp with Firestore instance

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -5,3 +5,9 @@ import com.google.firebase.firestore.FirebaseFirestore
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        NetworkRepositorySiswa(FirebaseFirestore.getInstance())
+    }
+}
