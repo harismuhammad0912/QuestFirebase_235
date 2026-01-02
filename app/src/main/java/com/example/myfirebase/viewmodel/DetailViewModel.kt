573b5dde4a504821153c5daf5e6cd2db9bba99bc
harismuhammad0912 <muhammadharis091204@gmail.com>
2026-01-03 03:22:58 +0700

    implementasi uiState untuk menampilkan data siswa secara

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -19,3 +19,11 @@ class DetailViewModel(
     // Mengambil ID siswa dari navigasi
     private val siswaId: String = checkNotNull(savedStateHandle["idSiswa"])
 
+    // Menampilkan data siswa secara realtime dari Firebase
+    val uiState: StateFlow<Siswa> = repositorySiswa.getSiswaById(siswaId)
+        .filterNotNull()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = Siswa() // Pastikan model Siswa punya nilai default
+        )
