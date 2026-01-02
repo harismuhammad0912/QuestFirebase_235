7958f8f70009c69a82325d14dece708b2f3e5c05
harismuhammad0912 <muhammadharis091204@gmail.com>
2026-01-03 03:20:25 +0700

    menambah fungsi getSiswaById dan insertSiswa pada repository

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -6,3 +6,9 @@ import kotlinx.coroutines.flow.Flow
 interface RepositorySiswa {
     // Fungsi untuk mengambil semua data siswa
     fun getAllSiswa(): Flow<List<Siswa>>
+
+    // Fungsi ini yang dicari EditViewModel (HARUS ADA)
+    fun getSiswaById(idSiswa: String): Flow<Siswa?>
+
+    // Fungsi untuk menambah data
+    suspend fun insertSiswa(siswa: Siswa)
